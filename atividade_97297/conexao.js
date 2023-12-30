590cca1716d199dc413ef2945cf3ea602285edbe
Diego Sampaio <samdiego80@gmail.com>
2023-12-30 18:00:20 -0300

    Entrega da atividade desenvolvimento extra 3.2 #97297.

---
+++ atividade_97297/conexao.js
@@ -0,0 +1,10 @@
+
+const MENSAGEM = (msg) => console.log(msg);
+
+for(i = 1; i<=3; i++) {
+    MENSAGEM('Conexão com um arquivo JavaScript');
+}
+
+MENSAGEM('Conexão com um arquivo JavaScript');
+MENSAGEM('Conexão com um arquivo JavaScript');
+MENSAGEM('Conexão com um arquivo JavaScript');
